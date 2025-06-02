8a5f414f7e68ef5137fcb536ebc27bdcbb9b9500
noyu38 <nojiyu.38@icloud.com>
2025-06-02 21:09:28 +0900

    first commit

---
+++ src/main/java/com/noyu/scheduling_tasks/SchedulingTasksApplication.java
@@ -0,0 +1,13 @@
+package com.noyu.scheduling_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTasksApplication.class, args);
+	}
+
+}
