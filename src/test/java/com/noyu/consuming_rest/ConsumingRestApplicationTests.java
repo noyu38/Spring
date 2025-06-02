70cfb6f07377dba69d9ee35888961dedec3e832f
noyu38 <nojiyu.38@icloud.com>
2025-06-02 21:37:57 +0900

    first commit

---
+++ src/test/java/com/noyu/consuming_rest/ConsumingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noyu.consuming_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
