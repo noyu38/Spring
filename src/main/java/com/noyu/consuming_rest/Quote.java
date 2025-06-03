d2fc37671918b675fb98d42eb59ed837126289bf
noyu38 <nojiyu.38@icloud.com>
2025-06-03 09:06:52 +0900

    なんか動かないから後回し

---
+++ src/main/java/com/noyu/consuming_rest/Quote.java
@@ -0,0 +1,6 @@
+package com.noyu.consuming_rest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Quote(String type, Value value) {}
---
+++ src/main/java/com/noyu/consuming_rest/Value.java
@@ -0,0 +1,6 @@
+package com.noyu.consuming_rest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Value(Long id, String quote) {}
